56b6f6225386f5c0ec4f805f0b40f45c6becc871
Becka <rebecca.lelew@gmail.com>
2018-12-07 15:01:31 +0000

    Adds ability to check if postcode is in whitelist

--- lib/postcode_checker.rb
+++ lib/postcode_checker.rb
@@ -2,12 +2,22 @@ require 'httparty'
 
 class Postcode_checker
 
+  def initialize
+    @whitelist_lsoas = ['Lambeth', 'Southwark']
+  end
+
   def output_lsoa(postcode)
     cleansed_pc = cleanse_postcode(postcode)
     postcode_info = HTTParty.get("https://api.postcodes.io/postcodes/#{cleansed_pc}")
     postcode_info["result"]["lsoa"]
   end
 
+  def whitelist?(postcode)
+    lsoa = output_lsoa(postcode)
+    lsoa_text = lsoa.split(' ').first
+    @whitelist_lsoas.include?(lsoa_text)
+  end
+  
   private
 
   def cleanse_postcode(postcode)
--- spec/postcode_checker_spec.rb
+++ spec/postcode_checker_spec.rb
@@ -1,9 +1,13 @@
 require './lib/postcode_checker'
 
 describe 'Postcode_checker' do
-  let(:parliament_postcode) { "SW1A0AA" }
-  let(:westminster_lsoa) { "Westminster 020C" }
-  let(:parliament_with_spaces) { " SW1A 0AA  " }
+  let(:parliament) { {postcode: "SW1A0AA", lsoa:"Westminster 020C"} }
+
+  let(:lambeth_palace) { {postcode: "SE17JU", lsoa: "Lambeth 036A"} }
+  let(:palace_with_spaces) { " SE1 7 JU  " }
+
+  let(:southwark_cathedral) { {postcode: "SE19DA", lsoa: "Southwark 002B"} }
+
 
   before do
     @checker = Postcode_checker.new
@@ -11,12 +15,25 @@ describe 'Postcode_checker' do
 
   describe '#output_lsoa' do
     it 'fetches the LSOA for a postcode' do
-      expect(@checker.output_lsoa(parliament_postcode)).to eq westminster_lsoa
+      expect(@checker.output_lsoa(lambeth_palace[:postcode])).to eq lambeth_palace[:lsoa]
     end
 
     it 'fetches the LSOA for a postcode with extra spaces in' do
-      expect(@checker.output_lsoa(parliament_with_spaces)).to eq westminster_lsoa
+      expect(@checker.output_lsoa(palace_with_spaces)).to eq lambeth_palace[:lsoa]
     end
   end
 
+  describe '#whitelist' do
+    it "returns true if a postcode is in Lambeth" do
+      expect(@checker.whitelist?(lambeth_palace[:postcode])).to eq true
+    end
+
+    it 'returns true if a postcode is in Southwark' do
+      expect(@checker.whitelist?(southwark_cathedral[:postcode])).to eq true
+    end
+
+    it 'whitelist returns false if a postcode is not in Lambeth or Southwark' do
+      expect(@checker.whitelist?(parliament[:postcode])).to eq false
+    end
+  end
 end
