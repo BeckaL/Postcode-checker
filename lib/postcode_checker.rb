c21be1247d79f80b762b1b217ffa53e1003c0629
Becka <rebecca.lelew@gmail.com>
2018-12-07 14:37:27 +0000

    User can enter postcode with any number of spaces

--- lib/postcode_checker.rb
+++ lib/postcode_checker.rb
@@ -3,8 +3,14 @@ require 'httparty'
 class Postcode_checker
 
   def output_lsoa(postcode)
-    postcode_info = HTTParty.get("https://api.postcodes.io/postcodes/#{postcode}")
+    cleansed_pc = cleanse_postcode(postcode)
+    postcode_info = HTTParty.get("https://api.postcodes.io/postcodes/#{cleansed_pc}")
     postcode_info["result"]["lsoa"]
   end
 
+  private
+
+  def cleanse_postcode(postcode)
+    postcode.gsub(' ', '')
+  end
 end
--- spec/postcode_checker_spec.rb
+++ spec/postcode_checker_spec.rb
@@ -1,13 +1,22 @@
 require './lib/postcode_checker'
 
 describe 'Postcode_checker' do
+  let(:parliament_postcode) { "SW1A0AA" }
+  let(:westminster_lsoa) { "Westminster 020C" }
+  let(:parliament_with_spaces) { " SW1A 0AA  " }
+
+  before do
+    @checker = Postcode_checker.new
+  end
 
   describe '#output_lsoa' do
     it 'fetches the LSOA for a postcode' do
-      parliament_postcode = "SW1A0AA"
-      westminster_lsoa = "Westminster 020C"
-      checker = Postcode_checker.new
-      expect(checker.output_lsoa(parliament_postcode)).to eq westminster_lsoa
+      expect(@checker.output_lsoa(parliament_postcode)).to eq westminster_lsoa
+    end
+
+    it 'fetches the LSOA for a postcode with extra spaces in' do
+      expect(@checker.output_lsoa(parliament_with_spaces)).to eq westminster_lsoa
     end
   end
+
 end
